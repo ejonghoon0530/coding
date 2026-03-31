94a9ec49801c9f1d390c93023f52acf211e608cc
LeeJongHun <ejonghoon0530@naver.com>
2026-03-31 12:09:57 +0900

    모스부호 디코더

---
+++ 0331.py
@@ -0,0 +1,142 @@
+import queue
+class ArrayQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.array = [None] * capacity
+        self.front = 0
+        self.rear = 0
+    def isEmpty(self):
+        return self.front == self.rear
+    def isFull(self):
+        return self.front == (self.rear + 1) % self.capacity
+    def enqueue(self, item):
+        if not self.isFull():
+            self.rear = (self.rear + 1) % self.capacity
+            self.array[self.rear] = item
+        else: pass
+    def dequeue(self):
+        if not self.isEmpty():
+            self.front = (self.front + 1) % self.capacity
+            return self.array[self.front]
+        else: pass
+    def peek(self):
+        if not self.isEmpty():
+            return self.array[(self.front + 1) % self.capacity]
+        else: pass
+    def size(self):
+        return (self.rear - self.front + self.capacity) % self.capacity
+    def display(self, msg):
+        print(msg, end='= [ ')
+        for i in range(self.front + 1, self.front + 1 + self.size()):
+            print(self.array[i % self.capacity], end=' ')
+        print("]")
+    
+
+class BTNode:
+    def __init__(self, elem, left = None, right = None):
+        self.data = elem
+        self.left = left
+        self.right = right
+def preorder(n):
+    if n is not None:
+        print(n.data, end = ' ')
+        preorder(n.left)
+        preorder(n.right)
+def inorder(n):
+    if n is not None:
+        inorder(n.left)
+        print(n.data, end = ' ')
+        inorder(n.right)
+def postorder(n):
+    if n is not None:
+        postorder(n.left)
+        postorder(n.right)
+        print(n.data, end = ' ')
+def levelorder(n):
+    queue = ArrayQueue(100)
+    queue.enqueue(root)
+    while not queue.isEmpty():
+        n = queue.dequeue()
+        if n is not None:
+            print(n.data, end = ' ')
+            queue.enqueue(n.left)
+            queue.enqueue(n.right)
+def count_node(n):
+    if n is None:
+        return 0
+    else:
+        return count_node(n.left) + count_node(n.right) + 1
+def calc_height(n):
+    if n is None:
+        return 0
+    hLeft = calc_height(n.left)
+    hRight = calc_height(n.right)
+    if(hLeft > hRight):
+        return hLeft + 1
+    else: return hRight + 1
+    
+            
+# d = BTNode('D', None, None)
+# e = BTNode('E', None, None)
+# b = BTNode('B', d, e)
+# f = BTNode('F', None, None)
+# c = BTNode('C', f, None)
+# root = BTNode('A', b, c)
+
+# print('\n In-Order: ', end = ' ')
+# inorder(root)
+# print('\n Pre-order: ' , end = ' ')
+# preorder(root)
+# print('\n Post-order: ' , end = ' ')
+# postorder(root)
+# print('\n Level-order: ' , end = ' ')
+# levelorder(root)
+# print()
+
+# print("노드의 개수 = %d개" % count_node(root))
+# print("트리의 높이 = %d" % calc_height(root))
+
+
+table = [('A', '.-'), ('B', '-...'), ('C', '-.-.'), ('D', '-..'), ('E', '.'), ('F', '..-.'),
+         ('G', '--.'), ('H', '....'), ('I', '..'), ('J', '.---'), ('K', '-.-'), ('L', '.-..'), ('M', '--'), ('N', '-.'), ('O', '---'),
+         ('P', '.--.'), ('Q', '--.-'), ('R', '.-.'), ('S', '...'), ('T', '-'), ('U', '..-'), ('V', '...-'), ('W', '.--'), ('X', '-..-'), ('Y', '-.--'), ('Z', '--..')]
+def encode(ch):
+    idx = ord(ch) - ord('A')
+    return table[idx][1]
+
+def make_morse_tree():
+    root = BTNode(None, None, None)
+    for tp in table:
+        code = tp[1]
+        node = root
+        for c in code:
+            if (c == '.'):
+                if node.left == None:
+                    node.left = BTNode(None, None, None)
+                node = node.left
+            elif (c == '-'):
+                if node.right == None:
+                    node.right = BTNode(None, None, None)
+                node = node.right
+        node.data = tp[0]
+    return root
+def decode(root, code):
+    node = root
+    for c in code:
+        if c == '.': node = node.left
+        elif c == '-': node = node.right
+    return node.data
+
+morseCodeTree = make_morse_tree()
+str = input("입력 문장: ")
+mlist = []
+for ch in str:
+    code = encode(ch)
+    mlist.append(code)
+print("Morse Code: ", mlist)
+print("Decoding: ", end = ' ')
+for code in mlist:
+    ch = decode(morseCodeTree, code)
+    print(ch, end = ' ')
+print()
+
