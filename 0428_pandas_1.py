0ac3e6e782199e265b5aa34c1b11dbc72e84f6aa
LeeJongHun <ejonghoon0530@naver.com>
2026-04-29 10:32:12 +0900

    0429 수업내용 추가

--- 0428_pandas_1.py
+++ 0428_pandas_1.py
@@ -92,14 +92,14 @@
 # print('\n')
 # print(position3)
 
-import pandas as pd
+# import pandas as pd
 
-exam_data = {'이름' : ['서준', '우현', '인아'],
-             '수학' : [90, 80, 70],
-             '영어' : [98, 89, 95],
-             '음악' : [85, 95, 100],
-             '체육' : [100, 90, 90]}
-df = pd.DataFrame(exam_data)
+# exam_data = {'이름' : ['서준', '우현', '인아'],
+#              '수학' : [90, 80, 70],
+#              '영어' : [98, 89, 95],
+#              '음악' : [85, 95, 100],
+#              '체육' : [100, 90, 90]}
+# df = pd.DataFrame(exam_data)
 # print(df)
 # print(type(df))
 # print('\n')
@@ -122,8 +122,167 @@ df = pd.DataFrame(exam_data)
 # print(math2)
 # print(type(math2))
 
-print(df.iloc[: : 2]) #0부터 전체행까지 2열간격으로
+# print(df.iloc[: : 2]) #0부터 전체행까지 2열간격으로
 
-print(df.iloc[0 : 3 : 2]) #0행부터 3 - 1 행까지 2열간격으로
+# print(df.iloc[0 : 3 : 2]) #0행부터 3 - 1 행까지 2열간격으로
 
-print(df.iloc[ : : -1]) #0행부터 전체행까지 -1 (역순으로)
+# print(df.iloc[ : : -1]) #0행부터 전체행까지 -1 (역순으로)
+
+# df.set_index('이름', inplace = True)
+# print(df)
+
+# a = df.loc['서준', '음악']
+# print(a)
+# b = df.iloc[0, 2]
+# print(b)
+
+# c = df.loc['서준', ['음악', '체육']]
+# print(c)
+# d = df.iloc[0, [2, 3]]
+# print(d)
+# e = df.loc['서준', '음악' : '체육']
+# print(e)
+# f = df.iloc[0, 2:]
+# print(f)
+
+# g = df.loc[['서준', '우현'], ['음악', '체육']]
+# print(g)
+# h = df.iloc[[0, 1], [2, 3]]
+# print(h)
+# i = df.loc['서준' : '우현', '음악' : '체육']
+# print(i)
+# j = df.iloc[0:2, 2:]
+# print(j)
+
+# print(df)
+# print('\n')
+
+# df.loc[3] = 0
+# print(df)
+# print('\n')
+
+# df.loc[4] = ['동규', 90, 80, 70, 60]
+# print(df)
+# print('\n')
+
+# df.loc['행5'] = df.loc[3]
+# print(df)
+
+# df.set_index('이름', inplace = True)
+# print(df)
+# print('\n')
+
+# df.iloc[0, 3] = 80
+# print(df)
+# print('\n')
+
+# df.loc['서준', '체육'] = 90
+# print(df)
+# print('\n')
+
+# df.loc['서준', '체육'] = 100
+# print(df)
+
+# df.loc['서준', ['음악', '체육']] = 50
+# print(df)
+# print('\n')
+
+# df.loc['서준', ['음악', '체육']] = 100, 50
+# print(df)
+
+# print(df)
+# print('\n')
+
+# df = df.transpose()
+# print(df)
+# print('\n')
+
+# df = df.T
+# print(df)
+
+# ndf = df.set_index(['이름'])
+# print(ndf)
+# print('\n')
+
+# ndf2 = ndf.set_index(['음악'])
+# print(ndf2)
+# print('\n')
+
+# ndf3 = ndf.set_index(['수학', '음악'])
+# print(ndf3)
+
+# import pandas as pd
+
+# dict_data = {'c0' : [1, 2, 3], 'c1' : [4, 5, 6], 'c2' : [7, 8, 9], 'c3' : [10, 11, 12], 'c4' : [13, 14, 15]}
+
+# df = pd.DataFrame(dict_data, index = ['a', 'c', 'b'])
+# print(df)
+# print('\n')
+
+# new_index = ['r0', 'r1', 'r2', 'r3', 'r4']
+# ndf = df.reindex(new_index)
+# print(ndf)
+# print('\n')
+
+# ndf2 = df.reindex(new_index, fill_value = 0)
+# print(ndf2)
+
+# ndf = df.reset_index()
+# print(ndf)
+
+# ndf = df.sort_index(ascending = False)
+# print(ndf)
+
+# ndf = df.sort_values(by = 'c1', ascending = False)
+# print(ndf)
+
+# import pandas as pd
+# import numpy as np
+
+# student1 = pd.Series({'국어' : np.nan, '영어' : 80, '수학' : 90})
+# student2 = pd.Series({'수학' : 80, '국어' : 90})
+
+
+# print(student1)
+# print('\n')
+
+# percentage = student1 / 100
+
+# print(percentage)
+# print('\n')
+# print(type(percentage))
+
+# print(student1)
+# print('\n')
+# print(student2)
+# print('\n')
+
+# sr_add = student1.add(student2, fill_value = 0)
+# sr_sub = student1.sub(student2, fill_value = 0)
+# sr_mul = student1.mul(student2, fill_value = 0)
+# sr_div = student1.div(student2, fill_value = 0)
+
+# result = pd.DataFrame([sr_add, sr_sub, sr_mul, sr_div], index = ['덧셈', '뺄셈', '곱셈', '나눗셈'])
+# print(result)
+
+import pandas as pd
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[:, ['age', 'fare']]
+print(df.tail())
+print('\n')
+print(type(df))
+print('\n')
+
+addition = df + 10
+print(addition.tail())
+print('\n')
+
+subtraction = addition - df
+print(subtraction.tail())
+print('\n')
+print(type(subtraction))
+
+
+
